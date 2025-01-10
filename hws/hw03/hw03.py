9d13b0d78f62dc6d984db8fbc1ca97c1ebf09ec4
Jinx <2807241764@qq.com>
2025-01-10 22:14:57 +0800

    update hw03

--- hws/hw03/hw03.py
+++ hws/hw03/hw03.py
@@ -25,6 +25,12 @@ def num_eights(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    if n==0:
+        return 0
+    if n%10==8:
+        return 1+num_eights(n//10)
+    else:
+        return num_eights(n//10)
 
 
 def digit_distance(n):
@@ -47,6 +53,13 @@ def digit_distance(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    if n<10:
+        return 0
+    if n<100:
+        return abs(n//10-n%10)
+    else:
+        return abs(n//10%10-n%10)+digit_distance(n//10)
+
 
 
 def interleaved_sum(n, odd_func, even_func):
@@ -71,6 +84,14 @@ def interleaved_sum(n, odd_func, even_func):
     True
     """
     "*** YOUR CODE HERE ***"
+    def sum(start):
+        if start >n:
+            return 0
+        elif start==n:
+            return odd_func(start)
+        else:
+            return odd_func(start)+even_func(start+1)+sum(start+2)
+    return sum(1)
 
 
 def next_smaller_dollar(bill):
@@ -107,6 +128,11 @@ def count_dollars(total):
     True
     """
     "*** YOUR CODE HERE ***"
+    # 组合问题 （元素可重复）
+    if total==0:
+        return 1
+    else:
+        
 
 
 def next_larger_dollar(bill):
