07baa27b646e48c89cfcf734b46e5e96ae375ef9
Jinx <2807241764@qq.com>
2025-01-26 20:48:43 +0800

    hw08 is over

--- hws/hw08/hw08.scm
+++ hws/hw08/hw08.scm
@@ -1,7 +1,31 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+    (cond 
+    ((null? s) #t)  ;s为空
+    ((null? (cdr s)) #t) ;s是单个元素
+    ((> (car s) (car (cdr s))) #f) ;条件不符
+    (else (ascending? (cdr s)))
+    )
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    (cond
+     ((null? s) '())
+     ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+     (else (my-filter pred (cdr s)))
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (cond
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (interleave lst2 (cdr lst1))))
+    )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) 
+    (if (null? s)
+        s
+        (cons (car s) (no-repeats(filter (lambda (x) (not (= (car s) x))) (cdr s))))
+    )
+)
